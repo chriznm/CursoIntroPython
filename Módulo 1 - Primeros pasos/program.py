8d3602e4a38a586dd655e2c415465c9c9152db6b
Chriz NM <chriznm@gmail.com>
2022-02-09 14:41:24 -0600

    [OnBoarding] Kata 1

---
+++ Módulo 1 - Primeros pasos/program.py
@@ -0,0 +1,42 @@
+sum = 1 + 2
+print(sum)
+#para ejecutar python program.py
+
+print('Hola desde la consola')
+# Impresión en pantalla print('Hola desde la consola')
+
+sum = 1 + 2 # 3
+product = sum * 2
+print(product)
+
+planetas_en_el_sistema_solar = 8 # int, plutón era considerado un planeta pero ya es muy pequeño
+distancia_a_alfa_centauri = 4.367 # float, años luz
+puede_despegar = True
+transbordador_que_aterrizo_en_la_luna = "Apollo 11" #string
+
+distancia_a_alfa_centauri = 4.367 # Parece un decimal flotante
+
+# Declaramos la variable
+distancia_a_alfa_centauri = 4.367
+
+# Descubrimos su tipo de dato
+print(type(distancia_a_alfa_centauri))
+
+left_side = 10
+right_side = 5
+left_side / right_side # 2
+
+from datetime import date
+
+print(date.today())
+
+print("Today's date is: " + str(date.today()))
+
+print("Bienvenido al programa de bienvenida")
+name = input("Introduzca su nombre ")
+print("Saludos: " + name)
+
+print("Calculadora")
+first_number = input("Primer número: ")
+second_number = input("Segundo número: ")
+print(first_number + second_number)
